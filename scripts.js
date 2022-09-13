ea16465125eca705ab88fd7c825df090224cd28b
Milad64sh <m.shalikarian@gmail.com>
2022-09-13 10:15:16 -0700

    js

--- scripts.js
+++ scripts.js
@@ -66,6 +66,62 @@ function createBox(item) {
   <img src="${image}" alt="${text}"/>
   <p class="info">${text}</p>
   `;
-  // todo - speak event
+  box.addEventListener('click', () => {
+    setTextMessage(text);
+    speakText();
+    // add active effect
+    box.classList.add('active');
+    setTimeout(() => {
+      box.classList.remove('active');
+    }, 800);
+  });
+
   main.appendChild(box);
 }
+// init speech synth
+const message = new SpeechSynthesisUtterance();
+
+// store the voices
+let voices = [];
+function getVoices() {
+  voices = speechSynthesis.getVoices();
+  voices.forEach((voice) => {
+    const option = document.createElement('option');
+    option.value = voice.name;
+    option.innerText = `${voice.name} ${voice.lang}`;
+    voicesSelect.appendChild(option);
+  });
+}
+// set text message
+function setTextMessage(text) {
+  message.text = text;
+}
+// speak the text
+function speakText() {
+  speechSynthesis.speak(message);
+}
+// set voice
+function setVoice(e) {
+  message.voice = voices.find((voice) => {
+    voice.name === e.target.value;
+  });
+}
+// voices change
+speechSynthesis.addEventListener('voiceschanged', getVoices);
+// toggle text box
+toggleBtn.addEventListener('click', () =>
+  document.getElementById('text-box').classList.toggle('show')
+);
+// close text box
+closeBtn.addEventListener('click', () =>
+  document.getElementById('text-box').classList.remove('show')
+);
+
+// change voice
+voicesSelect.addEventListener('change', setVoice);
+// read text button
+readBtn.addEventListener('click', () => {
+  setTextMessage(textarea.value);
+  speakText();
+});
+getVoices();
